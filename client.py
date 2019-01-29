df3ab2925ea0165d6d405e086f6ba6b20726de89
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-01-29 19:12:48 +0530

    2 Client Chat Done

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
-from socket import AF_INET,SOCK_STREAMs
+from socket import socket,AF_INET,SOCK_STREAM
 s = socket(AF_INET,SOCK_STREAM)
-s.connect(('',8080))
+host ="127.0.0.1"
+port = 8080
+s.connect((host,port))
 while True:
-    message = input("Your Message : ")
+    message = bytes(input("Your Message : "),'utf-8')
     s.send(message)
     print("Awaiting Reply")
     reply = s.recv(1024)
