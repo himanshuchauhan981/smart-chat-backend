a091e33a03005885b96b46ce17a6fe7749ec85d5
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-01-29 19:13:03 +0530

    Updated Server Python File

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 from socket import socket, AF_INET, SOCK_STREAM
-
-
+import threading
 host =""
 port = 8080
 s = socket(AF_INET,SOCK_STREAM)
 s.bind((host,port))
-s.listen(1)
-conn,addr = s.accept()
-print("Connected by", addr)
-while True:
-    print("Hello")
-    data = conn.recv(1024)
-    print("Received ",repr(data))
-    reply = input("Reply : ")
-    conn.sendall(reply)
-conn.close()
+s.listen(5)
+print("Server is running")
+
+def clientHandler():
+    conn,addr = s.accept()
+    print(addr," is connected")
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print("Received ",repr(data))
+
+#for i in range(5):
+t1 = threading.Thread(target=clientHandler)
+t2 = threading.Thread(target=clientHandler)
+t1.start()
+t2.start()
+#s.close()
+
