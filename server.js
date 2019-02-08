1dee06f9de9cf72c781562433e73149eb39b1b73
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-08 09:49:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,5 +6,11 @@ const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
 
+app.post('/chat/save_details/:data',function(request,response,next,username){
+   console.log(data);
+   return 200;
+
+});
+
 const port = process.env.PORT || 5000;
 server.listen(app.listen(port,console.log('Server is running at port 5000')));
