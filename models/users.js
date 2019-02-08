56e50cb31f904859c336af777c7da2a17ae7c886
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-08 23:22:23 +0530

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://smartChatAdmin:smartchat0018@ds125125.mlab.com:25125/smart_chat',{useNewUrlParser:true})
+
+const Schema = mongoose.Schema;
+
+const userSchema = mongoose.Schema({
+   'username':String,
+   'email':String,
+   'password':String
+});
+
+const database = mongoose.model('chat_user_details',userSchema);
+
+module.exports.database = database;
+
+module.exports.checkExistingUser = function(username,email,callback){
+   const query = { $or : [ { username:username }, { email:email } ] }
+   database.find(query,callback);
+}
