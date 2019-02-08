ee7ca15d26a9fe30c0428a8f3741f20a4260ea11
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-08 09:46:49 +0530

    Update app-routing.module.ts

--- chatApp/src/app/app-routing.module.ts
+++ chatApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 const routes: Routes = [
    {
@@ -15,6 +16,10 @@ const routes: Routes = [
    {
       path:'signup',
       component:SignupComponent
+   },
+   {
+      path:'chatroom',
+      component:ChatRoomComponent
    }
 
 ];
