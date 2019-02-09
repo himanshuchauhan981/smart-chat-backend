ceb73ecb63e9a89fd2de193e0eead976265aee5f
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-09 23:04:33 +0530

    Login Part Completed

--- chatApp/src/app/login/login.component.ts
+++ chatApp/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-   constructor() {}
+   loginForm:FormGroup
+   loginError: boolean =false
+   private loginErrorText:string
 
-   ngOnInit() {}
-   onSubmit(){
-      console.log("Its Working");
+   constructor(private formBuilder:FormBuilder, private _auth:AuthService, private router:Router) {}
+
+   ngOnInit(){
+      this.loginForm = this.formBuilder.group({
+         loginUsername:['',Validators.required],
+         loginPassword:['',Validators.required]
+      })
+   }
+   loginSubmit(event){
+      const target = event.target;
+      const loginUsername = target.querySelector('#loginUsernameID').value;
+      const loginPassword = target.querySelector('#loginPasswordID').value;
+      this._auth.checkUserDetail(loginUsername,loginPassword).subscribe(err =>{
+         this.loginError = true
+         this.loginErrorText = err.message;
+         if(err.isUserCorrect == true){
+            return this.router.navigate(['/chatroom']);
+         }
+      });
    }
 }
