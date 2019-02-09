e4a704710fcae3df38883608f0e6944f35d273b5
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-09 23:04:11 +0530

    Update auth.service.ts

--- chatApp/src/app/auth.service.ts
+++ chatApp/src/app/auth.service.ts
@@ -8,11 +8,18 @@ export class AuthService {
    constructor(private _http: HttpClient) { }
 
    get_user_details = function(username,email,password){
-      var data = {
+      const data = {
          'username':username,
          'email':email,
          'password':password
       }
       return this._http.post('/chat/save_details',data);
+   }
+   checkUserDetail = function(loginUsername, loginPassword){
+      const data = {
+         'loginUsername':loginUsername,
+         'loginPassword':loginPassword
+      }
+      return this._http.post('/chat/check_details',data);
    }
 }
