e87a3503bc11c8397dcb635b00afa3963bfcba39
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-11 00:16:43 +0530

    Update chat-room.component.ts

--- chatApp/src/app/chat-room/chat-room.component.ts
+++ chatApp/src/app/chat-room/chat-room.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthService} from '../auth.service';
+
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
-  styleUrls: ['./chat-room.component.css']
+  styleUrls: ['./chat-room.component.css'],
+  providers:[AuthService]
 })
 export class ChatRoomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService : AuthService) { }
 
   ngOnInit() {
   }
