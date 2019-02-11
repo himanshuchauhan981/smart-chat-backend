4c53c8da70c614fe512b8dfb15ee1d6ddc79c270
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-11 23:43:05 +0530

    Create chat.service.ts

---
+++ chatApp/src/app/chat.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import * as io  from 'socket.io-client';
+
+
+@Injectable({
+   providedIn: 'root'
+})
+export class ChatService {
+   //Creating new Connections from frontend side
+   private socket = io('http://localhost:1234');
+
+   constructor(){ }
+
+   //Function for login user to join community room
+   joinRoom(data){
+      this.socket.emit('join',data);
+   }
+
+   newUserJoined(){
+      let observable = new Observable<{user:string, message:string}>(observer=>{
+         this.socket.on('New user joined',(data)=>{
+            observer.next(data);
+         });
+         return () =>{this.socket.disconnect();}
+      });
+      return observable;
+   }
+
+   //Function for users to leave community room
+   leaveRoom(data){
+      this.socket.emit('leave',data)
+   }
+   //
+   userLeftRoom(){
+      let observable = new Observable<{user:string, message:string}>(observer=>{
+         this.socket.on('left room',(data)=>{
+            observer.next(data);
+         });
+         return () =>{this.socket.disconnect();}
+      });
+      return observable;
+   }
+
+   sendMessage(data){
+      this.socket.emit('message',data);
+   }
+
+   newMessageReceived(){
+      let observable = new Observable<{user:string, message:string}>(observer=>{
+         this.socket.on('new message',(data)=>{
+            observer.next(data);
+         });
+         return () =>{this.socket.disconnect();}
+      });
+      return observable;
+   }
+}
