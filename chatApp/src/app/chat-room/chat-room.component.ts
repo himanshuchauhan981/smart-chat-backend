d358720f49ab63f5f32eff0817fe2cd6afc27fc6
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-13 00:33:09 +0530

    Update chat-room.component.ts

--- chatApp/src/app/chat-room/chat-room.component.ts
+++ chatApp/src/app/chat-room/chat-room.component.ts
@@ -1,40 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {ChatService} from '../chat.service';
-import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
    selector: 'app-chat-room',
    templateUrl: './chat-room.component.html',
-   styleUrls: ['./chat-room.component.css'],
-   providers:[ChatService]
+   styleUrls: ['./chat-room.component.css']
 })
 export class ChatRoomComponent implements OnInit {
-   public user : string;
-   public messageArray:Array<{user:string, message:string}> = []
-   public messageText:string;
-
-   constructor(private _chatService : ChatService, private _route:ActivatedRoute){
-      this._route.queryParams.subscribe(params =>{
-         this.user = params["user"];
-      });
-
-      // When new user join, then previous user would get notification of
-      // new active user
-      this._chatService.newUserJoined().subscribe(data=> this.messageArray.push(data))
-      this._chatService.userLeftRoom().subscribe(data=> this.messageArray.push(data))
-      this._chatService.newMessageReceived().subscribe(data=>this.messageArray.push(data));
-   }
-   ngOnInit(){
-      //Login User Joins the community room
-      this._chatService.joinRoom({user:this.user})
-   }
-   leave(){
-      this._chatService.leaveRoom({user:this.user})
-   }
-   sendMessage(){
-      this._chatService.sendMessage({user:this.user, message:this.messageText});
-   }
-   private room: string = 'community';
+   constructor() {}
+   ngOnInit() {}
 
 }
