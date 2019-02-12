c9d6680cd4845f7aa334aca76f4e6600fecc4a25
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-13 00:32:26 +0530

    Update auth.service.ts

--- chatApp/src/app/auth.service.ts
+++ chatApp/src/app/auth.service.ts
@@ -2,27 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
    constructor(private _http: HttpClient) { }
 
-   get_user_details = function(username,email,password){
+   public usernameDetails: any;
+
+   get_user_details = function(username, email, password) {
       const data = {
-         'username':username,
-         'email':email,
-         'password':password
-      }
-      return this._http.post('/chat/save_details',data);
-   }
-   checkUserDetail = function(loginUsername, loginPassword){
+         'username': username,
+         'email': email,
+         'password': password
+      };
+      return this._http.post('/chat/save_details', data);
+   };
+   checkUserDetail = function(loginUsername, loginPassword) {
       const data = {
-         'loginUsername':loginUsername,
-         'loginPassword':loginPassword
-      }
-      return this._http.post('/chat/check_details',data);
-   }
+         'loginUsername': loginUsername,
+         'loginPassword': loginPassword
+      };
+      return this._http.post('/chat/check_details', data);
+   };
+
+   showRegisteredUsers = function() {
+     return this._http.get('/chat/getUsernames').subscribe(
+       res => console.log(res));
+   };
 }
