2bc00ed3b3de89ba44b052306a348c146ab9132d
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-13 00:33:48 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://smartChatAdmin:smartchat0018@ds125125.mlab.com:25125/smart_chat',{useNewUrlParser:true})
+mongoose.connect('mongodb://smartChatAdmin:smartchat0018@ds125125.mlab.com:25125/smart_chat',{useNewUrlParser:true});
 
-const Schema = mongoose.Schema;
 
 const userSchema = mongoose.Schema({
    'username':String,
@@ -14,11 +13,15 @@ const database = mongoose.model('chat_user_details',userSchema);
 module.exports.database = database;
 
 module.exports.checkExistingUser = function(username,email,callback){
-   const query = { $or : [ { username:username }, { email:email } ] }
+   const query = { $or : [ { username:username }, { email:email } ] };
    database.find(query,callback);
-}
+};
 
 module.exports.checkSavedUser = function(loginUsername, loginPassword, callback){
-   const query = {$and : [ { username:loginUsername }, { password:loginPassword } ] }
+   const query = {$and : [ { username:loginUsername }, { password:loginPassword } ] };
    database.find(query,callback);
-}
+};
+
+module.exports.getUsername = function(callback){
+   database.find(callback);
+};
