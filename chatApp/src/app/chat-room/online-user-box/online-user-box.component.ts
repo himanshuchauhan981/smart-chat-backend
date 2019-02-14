163542c836839a481c898fbe2cf31409e51f3add
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-14 22:44:14 +0530

    Update online-user-box.component.ts

--- chatApp/src/app/chat-room/online-user-box/online-user-box.component.ts
+++ chatApp/src/app/chat-room/online-user-box/online-user-box.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../auth.service';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../auth.service';
+import {ChatService} from './../../chat.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-online-user-box',
   templateUrl: './online-user-box.component.html',
-  styleUrls: ['./online-user-box.component.css']
+  styleUrls: ['./online-user-box.component.css'],
+  providers: [ChatService]
 })
 export class OnlineUserBoxComponent implements OnInit {
-
-  constructor(private _auth: AuthService) { }
+  public usernameDetails: any;
+  private activeuser: string;
+  constructor(private _auth: AuthService, private _chatService: ChatService, private _router: ActivatedRoute) {
+    this._router.queryParams.subscribe(params => {
+      this.activeuser = params['user'];
+    });
+  }
 
   ngOnInit() {
-    this._auth.showRegisteredUsers();
+    this._auth.showRegisteredUsers().subscribe(
+      res => this.usernameDetails = res);
   }
 
 }
