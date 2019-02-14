6a657f7b777cdf5332d0623dc5aaf2e18f48f090
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-14 22:44:01 +0530

    Update chat.service.ts

--- chatApp/src/app/chat.service.ts
+++ chatApp/src/app/chat.service.ts
@@ -1,66 +1,72 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 import * as io from 'socket.io-client';
 
 
 @Injectable({
-   providedIn: 'root'
+  providedIn: 'root'
 })
 export class ChatService {
-   // Creating new Connections from frontend side
-   private socket = io('http://localhost:1234');
+  constructor(public _http: HttpClient) {
+  }
 
-   constructor() { }
+  // Creating new Connections from frontend side
+  private socket = io('http://localhost:1234');
 
-   // Function for login user to join community room
-   joinRoom(data) {
-      this.socket.emit('join', data);
-   }
+  // Function for login user to join community room
+  joinRoom(data) {
+    this.socket.emit('join', data);
+  }
 
-   newUserJoined() {
-      const observable = new Observable<{user: string, message: string}>(observer => {
-         this.socket.on('New user joined', (data) => {
-            observer.next(data);
-         });
-         return () => {this.socket.disconnect(); };
+  newUserJoined() {
+    const observable = new Observable<{ user: string, message: string }>(observer => {
+      this.socket.on('New user joined', (data) => {
+        observer.next(data);
       });
-      return observable;
-   }
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+    return observable;
+  }
 
-   // Function for users to leave community room
-   leaveRoom(data) {
-     this.socket.emit('leave', data);
-   }
+  // Function for users to leave community room
+  leaveCommunityRoom(data) {
+    this.socket.emit('leave', data);
+  }
 
-   userLeftRoom() {
-      const observable = new Observable<{user: string, message: string}>(observer => {
-         this.socket.on('left room', (data) => {
-            observer.next(data);
-         });
-         return () => {this.socket.disconnect(); };
+  userLeftRoom() {
+    const observable = new Observable<{ user: string, message: string }>(observer => {
+      this.socket.on('left room', (data) => {
+        observer.next(data);
       });
-      return observable;
-   }
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+    return observable;
+  }
 
-   sendMessage(data) {
-      this.socket.emit('message', data);
-   }
+  sendMessage(data) {
+    this.socket.emit('message', data);
+  }
 
-   newMessageReceived() {
-      const observable = new Observable<{user: string, message: string}>(observer => {
-         this.socket.on('new message', (data) => {
-            observer.next(data);
-         });
-         return () => { this.socket.disconnect(); };
+  newMessageReceived() {
+    const observable = new Observable<{ user: string, message: string }>(observer => {
+      this.socket.on('new message', (data) => {
+        observer.next(data);
       });
-      return observable;
-   }
+      return () => {
+        this.socket.disconnect();
+      };
+    });
+    return observable;
+  }
 
-   removeUser() {
-     this.socket.on('disconnect', () => {
-       this.socket.disconnect();
-       console.log('hello');
-       console.log(this.socket.id + 'got disconnected');
-     });
-   }
+  removeUser() {
+    this.socket.disconnect()
+    console.log(this.socket.id + ' got disconnected');
+  }
+
 }
