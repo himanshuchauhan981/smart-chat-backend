5eb593d1161af9ea7ae9d5afa4970f029b797268
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-14 22:44:58 +0530

    Update auth.service.ts

--- chatApp/src/app/auth.service.ts
+++ chatApp/src/app/auth.service.ts
@@ -6,28 +6,49 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthService {
-   constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {
+  }
 
-   public usernameDetails: any;
+  get_user_details = function (username, email, password) {
+    const data = {
+      'username': username,
+      'email': email,
+      'password': password
+    };
+    return this._http.post('/chat/save_details', data);
+  };
 
-   get_user_details = function(username, email, password) {
-      const data = {
-         'username': username,
-         'email': email,
-         'password': password
-      };
-      return this._http.post('/chat/save_details', data);
-   };
-   checkUserDetail = function(loginUsername, loginPassword) {
-      const data = {
-         'loginUsername': loginUsername,
-         'loginPassword': loginPassword
-      };
-      return this._http.post('/chat/check_details', data);
-   };
+  checkUserDetail = function (loginUsername, loginPassword) {
+    const data = {
+      'loginUsername': loginUsername,
+      'loginPassword': loginPassword
+    };
+    return this._http.post('/chat/check_details', data);
+  };
 
-   showRegisteredUsers = function() {
-     return this._http.get('/chat/getUsernames').subscribe(
-       res => console.log(res));
-   };
+  showRegisteredUsers = function () {
+    return this._http.get('/chat/getUsernames');
+  };
+
+  sendMessageDetails = function (data) {
+    return this._http.post('/chat/saveChats', data);
+  };
+
+  communityChats = function () {
+    return this._http.get('/chat/getCommunityChats');
+  };
+
+  makeCurrentUserOnline = function(onlineUserName){
+    const data= {
+      'onlineUserName':onlineUserName
+    };
+    return this._http.post('/chat/makeOnline',data);
+  }
+
+  makeCurrentUserOffline = function(onlineUserName){
+    const data = {
+      'onlineUserName':onlineUserName
+    };
+    return this._http.post('/chat/makeOffline',data);
+  }
 }
