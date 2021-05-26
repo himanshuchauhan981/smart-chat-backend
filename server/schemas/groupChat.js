d14b74f80aafb71e4d29f16ff68dd50fb31116e8
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2021-05-26 21:50:11 +0530

    updated group chat schema and config

--- server/schemas/groupChat.js
+++ server/schemas/groupChat.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+let isReadDetails = {
+	userId: { type: mongoose.Types.ObjectId, ref: 'users' },
+	isReadDate: { type: Number, default: Date.now },
+};
 const groupChat = new Schema({
 	room: { type: mongoose.Types.ObjectId, required: true },
 	sender: { type: mongoose.Types.ObjectId, required: true, ref: 'users' },
 	text: { type: String, required: true },
 	createdDate: { type: Number, default: Date.now },
 	modifiedDate: { type: Number, default: Date.now },
-	isRead: { type: String, default: null },
+	isRead: { type: [isReadDetails], default: [] },
 });
 
 module.exports = mongoose.model('groupChat', groupChat);
